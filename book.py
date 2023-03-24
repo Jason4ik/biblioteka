f776aa883fcf4f2543e66acb8ac9a452c2588f06
Jason24 <y.r.ukrainianboy@icloud.com>
2023-03-24 12:05:38 +0100

    Book class

--- book.py
+++ book.py
@@ -1,2 +1,7 @@
+""" - book class (title, author, isbn, availability, .info())
+ """
 class Book:
-    pass
+    def __init__(self, title, author, isbn, availability = True):
+        self.title = title 
+        self.author = author
+        self.isbn = availability
