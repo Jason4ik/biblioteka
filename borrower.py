b1c082b877dd9f4badc275e17436ead1832c7da2
SeventhFret <maxxik007@gmail.com>
2023-03-24 12:01:50 +0100

    Imported classes from libraries.

---
+++ borrower.py
@@ -0,0 +1,2 @@
+class Borrower:
+    pass
--- library.py
+++ library.py
@@ -1,7 +1,13 @@
 from os import system
+from book import Book
+from borrower import Borrower
+
 
 system('clear')
 
 class Library:
     def __init__(self):
+        self.books = []
+
+    def add_book(self):
         pass
