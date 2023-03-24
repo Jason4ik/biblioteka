0194782d68203b4b4c2fa11e5ecd5b588b7ab34c
SeventhFret <maxxik007@gmail.com>
2023-03-24 13:32:17 +0100

    Implemented loan book

--- library.py
+++ library.py
@@ -14,5 +14,17 @@ class Library:
         self.books.append(Book(title, author))
 
     def loan_book(self, title, author):
-        print('Checking availability of book...')
+        print('Searching for a book...')
+        for ind, book in enumerate(self.books):
+            info = book.info()
+            if info["Title"] == title and info["Author"] == author:
+                print("Book is found!\nChecking availability...")
+                if info["Availability"]:
+                    print(f'Book is available!\nNow you borrowed book {info["Title"]} from {info["Author"]}!')
+                    self.books[ind].availability = False
+                    return 1
+                else:
+                    print("Sorry, book is already taken.")
+                    return 1
+        print("Sorry, but book doesn't exist in library.")
         
