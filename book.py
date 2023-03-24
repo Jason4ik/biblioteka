7ba33bf384507f121cad2b434cad08db8862f8f4
Jason24 <y.r.ukrainianboy@icloud.com>
2023-03-24 12:12:10 +0100

    Class Book2

--- book.py
+++ book.py
@@ -1,8 +1,9 @@
 """ - book class (title, author, isbn, availability, .info())
  """
+import random
 class Book:
-    def __init__(self, title, author, isbn, availability = True):
+    def __init__(self, title, author):
         self.title = title 
         self.author = author
-        self.isbn = isbn
-        self.availability = availability
+        self.isbn = random
+        self.availability = True
